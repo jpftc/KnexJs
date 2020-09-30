4c0f993a4e4c161839b87c05587827f75c8f6579
João Teixeira <jpteixeira1308@gmail.com>
2020-09-29 21:40:19 -0300

    Projeto Alterado 29-09-2020

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { innerJoin } = require("./database");
 // Carregando conexão com banco de dados
 const database = require("./database");
 
@@ -68,3 +69,76 @@ database.insert({ nome: "Dark Souls 3", preco: 60.00 }).into("games").then(data 
 // }).catch(err => {
 //     console.log(err);
 // });
+
+// Delete
+// database.where({id: 4}).delete().table("games").then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// Update
+// database.where({id: 5}).update({preco: 40}).table("games").then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// Order by
+// database.select().table("games").orderBy("preco","desc").then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+
+// Insert associado
+// database.insert({
+//     nome: "Bandai Namco",
+//     game_id: 6
+// }).table("estudios").then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// Join 1 p 1
+// database.select().table("games").innerJoin("estudios", "estudios.game_id", "games.id").where("games.id", 6).then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// jois 1 p m
+// database.select().table("games").innerJoin("estudios", "estudios.game_id", "games.id").where("games.id", 6).then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// joins m p m
+// database.select().table("games_estudios")
+//     .innerJoin("games", "games.id", "games_estudios.game_id")
+//     .innerJoin("estudios", "estudios.id", "games_estudios.estudio_id")
+//     .where("games.id", 6)
+//     .then(data => {
+//         console.log(data);
+//     }).catch(err => {
+//         console.log(err);
+//     });
+
+// Transactions, desfaz alterações caso haja falha no meio do processo de varias operações no banco de dados
+// async function testeTransacao() {
+//     try {
+//         await database.transaction(async trans => {
+//             await database.insert({nome: "CD Projekt"}).table("estudios");
+//             await database.insert({nome: "Pyxeralia"}).table("estudios");
+//             await database.insert({nome: "EA"}).table("estudios");
+//             await database.insert({nome: "Bethesda"}).table("estudios");
+//         });
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// testeTransacao();
